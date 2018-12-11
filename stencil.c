cb2625979e952e7beb7aed771447534925e6af04
Adam Sadiq <adamssdq@gmail.com>
2018-12-11 16:24:21 +0000

    trying communication

--- stencil.c
+++ stencil.c
@@ -69,6 +69,8 @@ int main(int argc, char *argv[])
   }
   double toc = wtime();
 
+  MPI_Finalize();
+
   // Output
   printf("------------------------------------\n");
   printf(" runtime: %lf s\n", toc - tic);
@@ -78,8 +80,6 @@ int main(int argc, char *argv[])
   free(image);
   free(buffer);
   free(bufferTemp);
-
-  MPI_Finalize();
 }
 
 float *getRow(float *segment, float *image, int start, int end)
@@ -96,61 +96,71 @@ float *getRow(float *segment, float *image, int start, int end)
 void stencil(const int nx, const int ny, float *restrict image, float *restrict tmp_image, int rank)
 {
 
-  if (rank == 0){
-    printf("rank 0\n");
+  int seven = 7;
+  int five = 5;
+  MPI_Status *status;
+
+  int fifteenHolder;
+
+  if (rank == 0)
+  {
+    MPI_Send(seven, 1, MPI_INT, 15, 0, MPI_COMM_WORLD);
+    printf("Sent\n");
   }
-  else if (rank == 15){
-    printf("rank 15\n");
+  else if (rank == 15)
+  {
+    MPI_Recv(fifteenHolder, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, status);
+    printf("Received\n");
   }
-  else {
+  else
+  {
     // printf("rank 0\n");
   }
 
+  //   //Corner cases
+  //   tmp_image[0] = image[0] * 0.6f + (image[nx] + image[1]) * 0.1f; //comment
+  //   tmp_image[nx - 1] = image[nx - 1] * 0.6f + (image[nx * 2 - 1] + image[nx - 2]) * 0.1f;
+  //   tmp_image[nx * ny - (nx)] = image[nx * ny - (nx)] * 0.6f + (image[nx * ny - (nx * 2)] + image[nx * ny - (nx - 1)]) * 0.1f;
+  //   tmp_image[nx * ny - 1] = image[nx * ny - 1] * 0.6f + (image[nx * ny - (nx + 1)] + image[nx * ny - 2]) * 0.1f;
 
-//   //Corner cases
-//   tmp_image[0] = image[0] * 0.6f + (image[nx] + image[1]) * 0.1f; //comment
-//   tmp_image[nx - 1] = image[nx - 1] * 0.6f + (image[nx * 2 - 1] + image[nx - 2]) * 0.1f;
-//   tmp_image[nx * ny - (nx)] = image[nx * ny - (nx)] * 0.6f + (image[nx * ny - (nx * 2)] + image[nx * ny - (nx - 1)]) * 0.1f;
-//   tmp_image[nx * ny - 1] = image[nx * ny - 1] * 0.6f + (image[nx * ny - (nx + 1)] + image[nx * ny - 2]) * 0.1f;
+  //   //top cases
 
-//   //top cases
+  //   for (int j = 1; j < nx - 1; ++j)
+  //   {
+  //     tmp_image[j] = image[j] * 0.6f + (image[j - 1] + image[j + 1] + image[j + nx]) * 0.1f;
+  //   }
 
-//   for (int j = 1; j < nx - 1; ++j)
-//   {
-//     tmp_image[j] = image[j] * 0.6f + (image[j - 1] + image[j + 1] + image[j + nx]) * 0.1f;
-//   }
+  //   //bottom cases
 
-//   //bottom cases
+  //   for (int j = 1; j < nx - 1; ++j)
+  //   {
+  //     tmp_image[nx * ny - nx + j] = image[nx * ny - (nx) + j] * 0.6f + (image[nx * ny - (nx) + j - 1] + image[nx * ny - (nx) + j + 1] + image[nx * ny - (2 * nx) + j]) * 0.1f;
+  //   }
 
-//   for (int j = 1; j < nx - 1; ++j)
-//   {
-//     tmp_image[nx * ny - nx + j] = image[nx * ny - (nx) + j] * 0.6f + (image[nx * ny - (nx) + j - 1] + image[nx * ny - (nx) + j + 1] + image[nx * ny - (2 * nx) + j]) * 0.1f;
-//   }
+  //   //1. left cases
 
-//   //1. left cases
+  //   for (int j = 1; j < nx - 1; ++j)
+  //   {
+  //     tmp_image[ny * j] = image[ny * j] * 0.6f + (image[(nx * j) + 1] + image[nx * (j - 1)] + image[nx * (j + 1)]) * 0.1f;
+  //   }
 
-//   for (int j = 1; j < nx - 1; ++j)
-//   {
-//     tmp_image[ny * j] = image[ny * j] * 0.6f + (image[(nx * j) + 1] + image[nx * (j - 1)] + image[nx * (j + 1)]) * 0.1f;
-//   }
+  //   //2. right cases
 
-//   //2. right cases
+  //   for (int j = 1; j < nx - 1; ++j)
+  //   {
+  //     tmp_image[nx * (j + 1) - 1] = image[nx * (j + 1) - 1] * 0.6f + (image[nx * j - 1] + image[nx * (j + 2) - 1] + image[nx * (j + 1) - 2]) * 0.1f;
+  //   }
 
-//   for (int j = 1; j < nx - 1; ++j)
-//   {
-//     tmp_image[nx * (j + 1) - 1] = image[nx * (j + 1) - 1] * 0.6f + (image[nx * j - 1] + image[nx * (j + 2) - 1] + image[nx * (j + 1) - 2]) * 0.1f;
-//   }
+  //   //3. middle cases
 
-//   //3. middle cases
-
-// #pragma omp simd
-//   for (int j = 0; j < (nx * (nx - 2)); j += nx)
-//   {
-//     for (int i = 1; i < ny - 1; ++i)
-//     {
-//       tmp_image[j + i + nx] = image[j + i + nx] * 0.6f + (image[j + i + nx + 1] + image[j + i + nx - 1] + image[j + i] + image[j + i + (nx * 2)]) * 0.1f;
-//     }
-//   }
+  // #pragma omp simd
+  //   for (int j = 0; j < (nx * (nx - 2)); j += nx)
+  //   {
+  //     for (int i = 1; i < ny - 1; ++i)
+  //     {
+  //       tmp_image[j + i + nx] = image[j + i + nx] * 0.6f + (image[j + i + nx + 1] + image[j + i + nx - 1] + image[j + i] + image[j + i + (nx * 2)]) * 0.1f;
+  //     }
+  //   }
 }
 
 // Create the input image
