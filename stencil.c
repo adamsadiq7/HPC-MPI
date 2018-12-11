c12943134f5d6ef765e78c96d37e4f98ee8a15d7
Adam Sadiq <adamssdq@gmail.com>
2018-12-11 18:10:32 +0000

    minor changes

--- stencil.c
+++ stencil.c
@@ -123,8 +123,8 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     //printf("tmp image %f, image %f\n", tmp_image[5+6*nx], image[5+6*nx] );
 
     printf("%d  %d\n", nx, ny );
-    for(int i = 0 ; i < 64; i++){
-     for( int j =0 ; j < 1024 ; j++){   
+    for(int i = 0 ; i < ny; i++){
+     for( int j =0 ; j < nx ; j++){   
 
        tmp_image[j+i*nx]  = image[j+i*nx] * 0.6f;
        
@@ -136,7 +136,7 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
       if(j>0)     {tmp_image[j+i*nx] += image[j-1+i*nx]*0.1;}
       if(j<nx-1)  {tmp_image[j+i*nx] += image[j+1 + i*nx]*0.1;}
       if(i==ny-1) {tmp_image[j+i*nx] += lastRowRecv[j]*0.1;}
-      printf("new tmp_image value %f \n ", tmp_image[j+i*nx]);
+      
 
      }
     }
@@ -157,17 +157,25 @@ void stencil(const int nx, const int ny, float *restrict image, float *restrict 
     MPI_Recv(firstRowRecv, nx , MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, status);
     MPI_Send(firstRowSend, nx , MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD );
 
-    // for(int i = 0 ; i < ny; i++){
-    //  for( int j =0 ; j< nx ; j++){       
-    //                                    tmp_image[j+i*nx] = image[j+i*nx] * 0.6;
-    //  if(i<1 && j>0 && j<nx-1)          tmp_image[j+i*nx] += firstRowRecv[j] + image[(j+1) + i*nx]*0.1 + 0.1*image[j + (i+1)*nx] +  0.1*image[(j-1) + i*nx];
-    //  else if(j<1)                      tmp_image[j+i*nx] += 0.1*image[j+1+i*nx] + 0.1*image[j+(i-1)*nx] + 0.1*image[j+(i+1)*nx];
-    //  else if(j>nx-2)                   tmp_image[j+i*nx] += 0.1*image[j-1+i*nx] + 0.1*image[j+(i-1)*nx] + 0.1*image[j+(i+1)*nx];
-    //  else if(i>ny-2 && j>0 && j<nx-1 ) tmp_image[j+i*nx] += 0.1*image[j + (i-1)*nx] + 0.1*image[j-1 + i*nx] + 0.1*image[j+1 + i*nx];
-    //  else {                            tmp_image[j+i*nx] += 0.1*image[j+1+i*nx] + 0.1*image[j-1 + i*nx] + 0.1*image[j + (i-1)*nx] + 0.1*0.1*image[j + (i+1)*nx]; } 
-    //  }
-    // }
-   
+    for (int i = 0; i < ny; i++)
+    {
+      for (int j = 0; j < nx; j++)
+      {
+        tmp_image[j + i * nx] = image[j + i * nx] * 0.6;
+        if (i < 1 && j > 0 && j < nx - 1)
+          tmp_image[j + i * nx] += firstRowRecv[j] + image[(j + 1) + i * nx] * 0.1 + 0.1 * image[j + (i + 1) * nx] + 0.1 * image[(j - 1) + i * nx];
+        else if (j < 1)
+          tmp_image[j + i * nx] += 0.1 * image[j + 1 + i * nx] + 0.1 * image[j + (i - 1) * nx] + 0.1 * image[j + (i + 1) * nx];
+        else if (j > nx - 2)
+          tmp_image[j + i * nx] += 0.1 * image[j - 1 + i * nx] + 0.1 * image[j + (i - 1) * nx] + 0.1 * image[j + (i + 1) * nx];
+        else if (i > ny - 2 && j > 0 && j < nx - 1)
+          tmp_image[j + i * nx] += 0.1 * image[j + (i - 1) * nx] + 0.1 * image[j - 1 + i * nx] + 0.1 * image[j + 1 + i * nx];
+        else
+        {
+          tmp_image[j + i * nx] += 0.1 * image[j + 1 + i * nx] + 0.1 * image[j - 1 + i * nx] + 0.1 * image[j + (i - 1) * nx] + 0.1 * 0.1 * image[j + (i + 1) * nx];
+        }
+      }
+    }
 
     // for(int i =0  ; i< 64 ; i++){
     //   for( int j= 0 ; j< 1024 ; j++){
