5a378a52eb9aa0d6d8677a44e3b8288506e3afdd
Adam Sadiq <adamssdq@gmail.com>
2018-12-13 03:07:24 +0000

    adding *

--- stencil.c
+++ stencil.c
@@ -10,7 +10,7 @@
 void stencil(const int nx, const int ny, float *image, float *tmp_image, int rank, int size);
 void init_image(const int nx, const int ny, float *image, float *tmp_image);
 void output_image(const char *file_name, const int nx, const int ny, float *image);
-float extractRow(float *inputArray, float *outputArray, int start, int end);
+float *extractRow(float *inputArray, float *outputArray, int start, int end);
 double wtime(void);
 int main(int argc, char *argv[])
 {
